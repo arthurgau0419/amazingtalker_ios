6c9cd2baadc0bfa6fe3acffe9960ef0ef2b88f14
Arthur Kao <arthurkao@pklotcorp.com>
2021-05-06 23:31:33 +0800

    initial commit

---
+++ amazingtalker-ios-ArthurKao/amazingtalker_ios_ArthurKaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  amazingtalker_ios_ArthurKaoApp.swift
+//  amazingtalker-ios-ArthurKao
+//
+//  Created by Arthur Kao on 2021/5/6.
+//
+
+import SwiftUI
+
+@main
+struct amazingtalker_ios_ArthurKaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
